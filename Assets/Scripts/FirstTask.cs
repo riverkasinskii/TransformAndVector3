a508eaed720cd779b6df71b105e8f6e5699e1eae
riverkasinskii <kinderplus8@gmail.com>
2022-10-19 11:56:04 +0300

    Second Commit

--- Assets/Scripts/FirstTask.cs
+++ Assets/Scripts/FirstTask.cs
@@ -3,71 +3,48 @@
 public class FirstTask : MonoBehaviour
 {    
     [SerializeField] private float _speed;
-    private readonly Vector3[] vector3 = new Vector3[4];
-    private Vector3 _target;
-    private bool _isForward;     
-        
+    private readonly Vector3[] vector3 = new Vector3[4];    
+    private int i = 0;
+
     private void Start()
-    {                
+    {
         vector3[0] = new Vector3(0, 0, 0);
         vector3[1] = new Vector3(10, 0, 0);
         vector3[2] = new Vector3(10, 0, 10);
-        vector3[3] = new Vector3(0, 0, 10);
-
-        _isForward = true;
-        _target = vector3[0];
-        transform.position = _target;
+        vector3[3] = new Vector3(0, 0, 10);       
+        transform.position = vector3[i];
     }
         
     private void Update()
-    {        
-        transform.position = Vector3.MoveTowards(transform.position, _target, Time.deltaTime * _speed);
-                
-        if (transform.position == _target)
+    {
+        Running();        
+    }
+
+    private void Running()
+    {
+        if (i < vector3.Length - 1)
         {
-            if (_isForward == true)
+            transform.position = Vector3.MoveTowards(transform.position, vector3[i + 1], Time.deltaTime * _speed);
+
+            transform.LookAt(vector3[i + 1]);
+
+            if (transform.position == vector3[i + 1])
             {
-                if (_target == vector3[0])
-                {
-                    _target = vector3[1];
-                }
-                else if (_target == vector3[1])
-                {
-                    _target = vector3[2];
-                }
-                else if (_target == vector3[2])
-                {
-                    _target = vector3[3];
-                }
-                else if (_target == vector3[3])
-                {
-                    _target = vector3[0];
-                    _isForward = false;
-                }                
+                transform.position = vector3[i + 1];
+                i++;
             }
-            else
-            {
-                if (_target == vector3[0])
-                {
-                    _target = vector3[3];
-                }
-                else if (_target == vector3[3])
-                {
-                    _target = vector3[2];
-                }
-                else if (_target == vector3[2])
-                {
-                    _target = vector3[1];
-                }
-                else if (_target == vector3[1])
-                {
-                    _target = vector3[0];
-                    _isForward = true;
-                }                
-            }      
-            
-            transform.LookAt(_target);
         }
-        
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, vector3[0], Time.deltaTime * _speed);
+
+            transform.LookAt(vector3[0]);
+
+            if (transform.position == vector3[0])
+            {                
+                i = 0;
+                transform.position = vector3[i];
+            }
+        }
     }
 }
